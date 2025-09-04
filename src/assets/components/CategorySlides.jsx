4bbc57eee434473f1b24e67f93e31c960b9edb4f
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-09-04 12:06:52 +0530

    add

--- src/assets/components/CategorySlides.jsx
+++ src/assets/components/CategorySlides.jsx
@@ -1,29 +1,136 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { MdClose } from "react-icons/md";
-import {Maincategories} from "../data"
+import { Maincategories } from "../data";
+import axios from "axios";
+import { dataContext } from "../../App";
+import { toast, ToastContainer } from "react-toastify";
 
 const CategorySlides = () => {
+  const { api, token } = useContext(dataContext);
+  const [selectedSlides, setSelectedSlides] = useState([]);
+  const [categorySlides, setCategorySlides] = useState({});
+  const [loader, setLoader] = useState(true);
+  const [spin, setSpin] = useState(false);
+
+  // fetch category slides
+  const getCateSlides = async () => {
+    try {
+      const res = await axios.get(`${api}/api/get-category-slides`);
+      if (res) {
+        setCategorySlides(
+          res.data?.categorySlides === null ? [] : res.data?.categorySlides
+        );
+         
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoader(false);
+    }
+  };
+
+  useEffect(() => {
+    getCateSlides();
+  }, []);
+
+  useEffect(() => {
+    if (categorySlides?.categorySlides?.length <= 0) {
+      setSelectedSlides([]);
+    } else if (categorySlides?.categorySlides?.length >= 1) {
+      setSelectedSlides(categorySlides?.categorySlides);
+    }
+  }, [categorySlides]);
+
+  // select category input handler
+  const selectCategorySlides = (e) => {
+    const selectedInput = e.target.value;
+    if (!selectedSlides?.includes(selectedInput)) {
+      setSelectedSlides([...selectedSlides, selectedInput]);
+    }
+  };
+
+  console.log(categorySlides);
+
+  console.log(selectedSlides);
+
+  // update category slides
+  const updateCategorySlides = async () => {
+    try {
+      setSpin(true);
+      let res;
+      if (!categorySlides?._id) {
+        res = await axios.post(
+          `${api}/api/add-category-slides`,
+          { categorySlides: selectedSlides },
+          {
+            headers: {
+              token: token,
+            },
+          }
+        );
+      } else {
+        res = await axios.put(
+          `${api}/api/update-category-slides/${categorySlides._id}`,
+          { categorySlides: selectedSlides },
+          {
+            headers: {
+              token: token,
+            },
+          }
+        );
+      }
+
+      if (res) {
+        toast.success("updated successfully");
+        getCateSlides();
+      }
+    } catch (error) {
+      console.error(error);
+      if (error.response.data.message && error) {
+        if (error.status === 400) {
+          toast.warn(error.response.data.message);
+        }
+      } else {
+        toast.error("please try again not updated");
+      }
+    } finally {
+      setSpin(false);
+    }
+  };
+
+  if (loader) {
+    return (
+      <div className="flex h-screen font-bold text-[1rem] justify-center items-center">
+        Loading...
+      </div>
+    );
+  }
+
   return (
     <div className="mt-[6.1rem] p-3 lg:p-5 pb-9">
-      <div className="w-full flex flex-col md:flex-row gap-5 md:items-center md:justify-around">
+      <ToastContainer position="top-center" theme="dark" />
+      <div className="w-full flex flex-col md:flex-row gap-5 md:justify-around">
         <div className="md:w-1/2">
+          {/* all categories drop down  */}
           <label
-            htmlFor="productCategoryName"
+            htmlFor="category"
             className="block text-sm/6 md:text-[1rem] font-medium text-gray-900"
           >
-          Select Category for Slides 
+            Select Category for Slides
           </label>
           <div className="mt-2 grid grid-cols-1">
             <select
               required
-              name="productCategoryName"
-              id="productCategoryName"
+              name="category"
+              onChange={selectCategorySlides}
+              id="category"
               className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1  outline-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
             >
               <option disabled value="">
-                Select the options
+                Select the category
               </option>
               {Maincategories?.map((item, index) => (
+                
                 <option key={index} className={`capitalize `} value={item}>
                   {item}
                 </option>
@@ -45,12 +152,37 @@ const CategorySlides = () => {
           </div>
         </div>
 
+        {/* render category slides list  */}
         <div className=" md:w-1/3">
-        <h5 className="text-gray-800 mb-3 font-semibold">Selected Categories</h5>
-          <div className=" flex justify-around items-center gap-3 bg-blue-900 text-white rounded-full px-3 h-8">
-            <MdClose className="cursor-pointer" size={19} />
-          </div>
+          <h5 className="text-gray-800 mb-3 font-semibold">
+            Selected Categories
+          </h5>
+          <ol className="flex flex-col gap-3">
+            {selectedSlides?.map((cate, index) => (
+              <li
+                key={index}
+                className=" flex justify-between items-center gap-3 bg-blue-900 text-white rounded-full px-3 h-8"
+              >
+                {index + 1}. {cate}
+                <MdClose className="cursor-pointer" size={19} />
+              </li>
+            ))}
+          </ol>
         </div>
+      </div>
+      <div className="text-center mt-5 pt-5  border-t border-gray-400">
+        {spin ? (
+          <button className="bg-blue-500 hover:bg-blue-600 font-semibold py-1 text-white rounded w-1/3">
+            Submitting...
+          </button>
+        ) : (
+          <button
+            onClick={updateCategorySlides}
+            className="bg-blue-500 hover:bg-blue-600 font-semibold py-1 text-white rounded w-1/3"
+          >
+            Submit
+          </button>
+        )}
       </div>
     </div>
   );
